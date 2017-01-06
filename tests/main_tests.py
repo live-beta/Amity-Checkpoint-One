2a981856a43878afafabe4132ca3940a48174a2a
live-beta <wanjala.sam.s@gmail.com>
2017-01-06 10:46:43 +0300

    (Update) Empty test functionstests/main_test.py

---
+++ tests/main_tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+class amity_tests(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+    def test_create_room(self):
+        pass
+    def test_add_person(self):
+        pass
+    def test_reallocate_person(self):
+        pass
+    def test_load_people(self):
+        pass
+    def test_print_allocations(self):
+        pass
+    def test_print_unallocations(self):
+        pass
+    def test_print_room(self):
+        pass
+    def test_save_state(self):
+        pass
+    def test_load_state(self):
+        pass

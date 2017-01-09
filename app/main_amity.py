1ba62d4744614d0beed1df69522a72c1c8d41aaa
live-beta <wanjala.sam.s@gmail.com>
2017-01-09 12:30:55 +0300

    Directory restructuring

---
+++ app/main_amity.py
@@ -0,0 +1,35 @@
+class Person(object):
+    """docstring for Person."""
+    def __init__(self, arg):
+        super(Person, self).__init__()
+        self.arg = arg
+        
+class Fellow(object):
+    """docstring for Fellow."""
+    def __init__(self, arg):
+        super(Fellow, self).__init__()
+        self.arg = arg
+
+class Staff(object):
+    """docstring for Staff."""
+    def __init__(self, arg):
+        super(Staff, self).__init__()
+        self.arg = arg
+
+class Room(object):
+    """docstring for Room."""
+    def __init__(self, arg):
+        super(Room, self).__init__()
+        self.arg = arg
+
+class Office(object):
+    """docstring for Office."""
+    def __init__(self, arg):
+        super(Office, self).__init__()
+        self.arg = arg
+
+class LivingSpace(object):
+    """docstring for LivingSpace."""
+    def __init__(self, arg):
+        super(LivingSpace, self).__init__()
+        self.arg = arg
--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,25 +1,49 @@
 import unittest
+from app.main_amity import Room
 
-class amity_tests(object):
-    """docstring for ."""
+class amity_tests(unittest.TestCase):
+    """Tests for Amity functions ."""
+
     def __init__(self, arg):
-        super(, self).__init__()
+        super(amity_tests, self).__init__()
         self.arg = arg
+
     def test_create_room(self):
-        pass
+        "Creating room Validation"
+        self.assertIsInstance(self.createRoom('name','size','occupants','Y'),'Staff')
+        self.assertIsInstance(self.createRoom('name','size','occupants','N'), 'Fellow')
+
+        "Test For room creation"
+        self.assertIsInstance(self)
+
+        "Input Validation Test"
+        self.assertEqual(self.createRoom('name','size','occupants'),'Invalid')
+
+
+
     def test_add_person(self):
+        #write tests that will validate the entries of the users as to be in the correct format
+        #write a systen test to differenciate between a staff and fellow
+
         pass
+
     def test_reallocate_person(self):
         pass
+
     def test_load_people(self):
         pass
+
     def test_print_allocations(self):
         pass
+
     def test_print_unallocations(self):
         pass
+
     def test_print_room(self):
         pass
+
     def test_save_state(self):
         pass
+
     def test_load_state(self):
         pass

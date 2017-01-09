07fc5aececdfb2d20826016f4a72faba42a50d7a
live-beta <wanjala.sam.s@gmail.com>
2017-01-09 15:51:36 +0300

    Remove redundant class inits and debug createRoom unit test

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,34 +1,25 @@
 import unittest
-from app.main_amity import Room
+from app.main_amity import Room,Person
 
 class amity_tests(unittest.TestCase):
     """Tests for Amity functions ."""
 
-    def __init__(self, arg):
-        super(amity_tests, self).__init__()
-        self.arg = arg
-
-    def test_create_room(self):
+    def test_Room(self):
         "Creating room Validation"
-        self.assertIsInstance(self.createRoom('name','size','occupants','Y'),'Staff')
-        self.assertIsInstance(self.createRoom('name','size','occupants','N'), 'Fellow')
 
-        "Test For room creation"
-        self.assertIsInstance(self)
+        self.assertIsInstance(Room.createRoom('name',7,-1,'Y'),'Capacity Problems')
+        self.assertIsInstance(Room.createRoom(1234,'size','Y'),'Invalid Naming')
 
         "Input Validation Test"
-        self.assertEqual(self.createRoom('name','size','occupants'),'Invalid')
-
-
+        self.assertEqual(Room.createRoom('name','size','occupants'),'Invalid')
+        #inputs should be block letters
 
     def test_add_person(self):
-        #write tests that will validate the entries of the users as to be in the correct format
-        #write a systen test to differenciate between a staff and fellow
 
-        pass
-
-    def test_reallocate_person(self):
-        pass
+        "Test for successful creation"
+        self.assertTrue(Person.add_person('name','gender','category','employee_num'))
+        self.raises
+         #having relevant variables, name, genfer
 
     def test_load_people(self):
         pass

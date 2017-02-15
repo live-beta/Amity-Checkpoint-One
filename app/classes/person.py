f6e26acfc1ab3763f6bbae816a4c74164bcf8cc3
live-beta <sammy.wanjala@andela.com>
2017-02-15 16:01:39 +0300

    [Update] Applyting pep8 standard in code format

--- app/classes/person.py
+++ app/classes/person.py
@@ -1,16 +1,17 @@
 import os
 from room import Room
 
+
 class Person(object):
 
     """docstring for Person."""
     people_list = {}
-    room_allocation_list ={}
+    room_allocation_list = {}
 
     def __init__(self):
         pass
 
-    def add_person(self,person_name,person_job,wants_room,employee_number):
+    def add_person(self, person_name, person_job, wants_room, employee_number):
         """
         Function that adds a new person to the system
 
@@ -37,24 +38,26 @@ class Person(object):
                 person['name'] = person_name.upper()
                 person['employee_num'] = employee_number
                 person['job'] = person_job.upper()
-                person['wants_room']= wants_room
+                person['wants_room'] = wants_room
 
                 # Allocate working space to person
                 # work_space = obj_room.allocate_work_space()
 
                 person['work_space'] = obj_room.allocate_work_space()
 
-                if wants_room.upper() == 'Y' and person_job.upper() =='FELLOW':
+                if wants_room.upper() == 'Y' and person_job.upper() \
+                        == 'FELLOW':
 
                     # If Fellow wants accomodation
 
                     room_name = obj_room.allocate_living_room()
 
-                    #work_space = obj_room.allocate_work_space()
+                    # work_space = obj_room.allocate_work_space()
 
                     person['room'] = room_name.upper()
 
-                elif wants_room.upper() == 'Y' and person_job.upper() =='STAFF':
+                elif wants_room.upper() == 'Y' and person_job.upper() \
+                        == 'STAFF':
 
                     # An instance where Staff requests for accomodation
 
@@ -85,15 +88,15 @@ class Person(object):
 
         """
         # Open file with people's data
-        myfile = open("people.txt","r")
+        myfile = open("people.txt", "r")
         data = ""
         lines = myfile.readlines()
         for line in lines:
-            data = data + line.strip();
+            data = data + line.strip()
 
         return data
 
-    def reallocate_person(self,emp_no,room_name):
+    def reallocate_person(self, emp_no, room_name):
 
         """
         Placing person in a different room.
@@ -127,11 +130,13 @@ class Person(object):
 
                 # If the fellow being assigned did not require a room
 
-                option =raw_input('The fellow did not require a room before, are you sure you want to proceed? Y|N')
+                option = raw_input('The fellow did not require a room before,\
+                        are you sure you want to proceed? Y|N')
 
                 if option == 'Y':
 
-                    Person.people_list[emp_no]['room'] = room_reassigned['name']
+                    Person.people_list[emp_no]['room'] \
+                        = room_reassigned['name']
 
                 elif option == 'N':
                     return 'The fellow does not require a room'
@@ -140,9 +145,8 @@ class Person(object):
 
                     return 'Invalid input'
 
-
         elif room_reassigned['category'] == 'LIVING' \
-            and Person.people_list[emp_no]['job'] == 'STAFF':
+                and Person.people_list[emp_no]['job'] == 'STAFF':
 
             return 'Staff not allowed to have accomodation on site'
 
@@ -157,6 +161,7 @@ class Person(object):
         unallocated_list = []
         return unallocated_list
 
+
 class Fellow(Person):
 
     """docstring for FELLOW, subclass of Person."""
@@ -165,7 +170,7 @@ class Fellow(Person):
         super(fellow, self).__init__()
         self.arg = arg
 
-    def allocate_living_room(self,emp_no):
+    def allocate_living_room(self, emp_no):
         pass
 
 
